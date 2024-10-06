e387381b5aeb5b59ddbf4cd49e94b25d418944b1
DESKTOP-HVE1QIR\Usuario <jesusmaguerrero05@gmail.com>
2024-10-06 22:44:02 +0200

    segundo commit

---
+++ clima.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+month = int(input("ingrese el numero del mes:"))
+
+if month == 1 or month == 2 or month == 3:
+  print('Winter 🌨️')
+elif month == 4 or month == 5 or month == 6:
+  print('Spring 🌱')
+elif month == 7 or month == 8 or month == 9:
+  print('Summer 🌻')
+elif month == 10 or month == 11 or month == 12:
+  print('Autumn 🍂')
+else:
+  print('Invalid')

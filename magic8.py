490a641e8e5810c84ef3b7d9d3b5903c06b5d757
DESKTOP-HVE1QIR\Usuario <jesusmaguerrero05@gmail.com>
2024-10-06 22:36:24 +0200

    primer commit!

---
+++ magic8.py
@@ -0,0 +1,27 @@
+# Write code below 💖
+import random
+
+pregunta = input("Di tu pregunta:")
+respuesta = random.randint(1,9)
+if respuesta == 1:
+  print("seguro")
+elif respuesta == 2:
+  print("Es decididamente así")
+elif respuesta == 3:
+  print("Sin duda")
+elif respuesta == 4:
+  print("Respuesta confusa")
+elif respuesta == 5:
+  print("Pregunta de nuevo más tarde")
+elif respuesta == 6:
+  print("Mejor no decirte ahora")
+elif respuesta == 7:
+  print("Mis fuentes dicen que no")
+elif respuesta == 8:
+  print("Las perspectivas no son tan buenas")
+elif respuesta == 9:
+  print("Muy dudoso.")
+else:
+  print("creo que no entedi")
+
+  
